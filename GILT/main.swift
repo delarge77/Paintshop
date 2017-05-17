17790023d9653a6de3b41f0165374ae0c870cb9d
delarge77 <lastripper@gmail.com>
2017-05-17 14:40:41 +0100

    Initial Commit

---
+++ GILT/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GILT
+//
+//  Created by Alessandro dos Santos Pinto on 17/05/2017.
+//  Copyright © 2017 Alessandro dos Santos Pinto. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
